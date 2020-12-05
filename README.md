fe969565b866d1e278a3cdb5cfdebbaf9fb1ff6f
Maikpwwq <mariasf@correo.udistrital.edu.co>
2020-12-05 10:11:07 -0500

    PlantillaBootstrap

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # Semana-2-28
 Sitio con componentes reutilizables de VUE y uso APIs para consulta de datos
+
+## Actividades Semana 2
+
+1.	Transformar las secciones a componente de Vue
+2.	Usar la API Colombia News para consultar la sección de noticias
+3.	Hacer la consultar de datos de los integrantes desde un archivo JSON
+4.  Aplicar estilos de Bootstrap 4 a cada una de las etiquetas
+
+
+## Project scripts
+```
+npm install
+npm run serve
+npm run build
+npm run lint
+```
---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <MainNavigation/>
+    <OfertServices/>
+    <ColombiaNews/>
+    <TeamMembers/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+import MainNavigation from "./components/MainNavigation";
+import OfertServices from "./components/MainNavigation";
+import ColombiaNews from "./components/MainNavigation";
+import TeamMembers from "./components/MainNavigation";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+    MainNavigation,
+    OfertServices,
+    ColombiaNews,
+    TeamMembers
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
