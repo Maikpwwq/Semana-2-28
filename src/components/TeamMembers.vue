2853c3fa842b26694f50d5fd6386e83535c4fdb0
heywinmeneses <heywin1@gmail.com>
2020-12-05 22:37:00 -0500

    second

--- src/components/TeamMembers.vue
+++ src/components/TeamMembers.vue
@@ -7,13 +7,52 @@
                     <h3 class="section-subheading text-muted">Tripulacion MisionTic2022 #elegidosparatriunfar.</h3>
                 </div>
                 <div class="row">
-                <!-- Importar aqui las tarjetas y ciclar -->
+                    <div class="col-lg-4">
+                        <div class="team-member">
+                            <img class="mx-auto rounded-circle" src="../assets/img/integrantes/carlos.png" alt="Carlos Ruiz" title="Carlos Ruiz"/>
+                            <h4>Charlitos</h4>
+                            <p class="text-muted">Lead Designer</p>
+                            <a class="btn btn-dark btn-social mx-2" href="#!"><i class="fab fa-twitter"></i></a>
+                            <a class="btn btn-dark btn-social mx-2" href="#!"><i class="fab fa-facebook-f"></i></a>
+                            <a class="btn btn-dark btn-social mx-2" href="#!"><i class="fab fa-linkedin-in"></i></a>
+                        </div>
+                    </div>
+                    <div class="col-lg-4">
+                        <div class="team-member">
+                            <img class="mx-auto rounded-circle" src="../assets/img/integrantes/daniel.png" alt="Daniel Eslava" title="Daniel Eslava"/>
+                            <h4>Daniel Eslava</h4>
+                            <p class="text-muted">Lead Marketer</p>
+                            <a class="btn btn-dark btn-social mx-2" href="#!"><i class="fab fa-twitter"></i></a>
+                            <a class="btn btn-dark btn-social mx-2" href="#!"><i class="fab fa-facebook-f"></i></a>
+                            <a class="btn btn-dark btn-social mx-2" href="#!"><i class="fab fa-linkedin-in"></i></a>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-4">
+                        <div class="team-member">
+                            <img class="mx-auto rounded-circle" src="../assets/img/integrantes/heywin.jpeg" alt="Heywin Meneses" title="Heywin Meneses"/>
+                            <h4>Heywin Meneses</h4>
+                            <p class="text-muted">Lead Developer</p>
+                            <a class="btn btn-dark btn-social mx-2" href="#!"><i class="fab fa-twitter"></i></a>
+                            <a class="btn btn-dark btn-social mx-2" href="#!"><i class="fab fa-facebook-f"></i></a>
+                            <a class="btn btn-dark btn-social mx-2" href="#!"><i class="fab fa-linkedin-in"></i></a>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-4">
+                        <div class="team-member">
+                            <img class="mx-auto rounded-circle" src="../assets/img/integrantes/mike.jpeg" alt="Michael Arias" title="Michael Arias"/>
+                            <h4>Mike_col</h4>
+                            <p class="text-muted">Lead Developer</p>
+                            <a class="btn btn-dark btn-social mx-2" href="#!"><i class="fab fa-twitter"></i></a>
+                            <a class="btn btn-dark btn-social mx-2" href="#!"><i class="fab fa-facebook-f"></i></a>
+                            <a class="btn btn-dark btn-social mx-2" href="#!"><i class="fab fa-linkedin-in"></i></a>
+                        </div>
+                    </div>
 
                 </div>
                 <div class="row">
-                    <div class="col-lg-8 mx-auto text-center">
-                        <p class="large text-muted">El Motivante del desarrollo.</p>
-                    </div>
+                    <div class="col-lg-8 mx-auto text-center"><p class="large text-muted">El Motivante del desarrollo.</p></div>
                 </div>
             </div>
     </section>
