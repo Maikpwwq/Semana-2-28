a007b4913aeb2e9a8d6011a6335267c0b49c68db
heywinmeneses <heywin1@gmail.com>
2020-12-06 17:12:49 -0500

    tird

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,39 @@
 <template>
   <div id="app">
     <img alt="Welcome team 28" src="./assets/img/MisionTIC.jpg" />
-    <MainNavigation msg="Sprint semana 2"/>
-    <OfertServices/>
-    <ColombiaNews/>
-    <TeamMembers/>
-    <AboutUs/>
-    <OurClients/>
-    <FormContact/>
-    <FooterContent/>
+    <MainNavigation msg="Sprint semana 2" />
+    <OfertServices />
+    <ColombiaNews />
+
+    <section class="page-section bg-light" id="team">
+      <div class="container">
+        <div class="text-center">
+          <h2 class="section-heading text-uppercase">Un increible equipo</h2>
+          <h3 class="section-subheading text-muted">
+            Tripulacion MisionTic2022 #elegidosparatriunfar.
+          </h3>
+        </div>
+        <div class="row">
+          <div id="contenedor" v-for="(item, index) of equipo" :key="index">
+            <team-card :member="item"></team-card>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-lg-8 mx-auto text-center">
+            <p class="large text-muted">El Motivante del desarrollo.</p>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <AboutUs />
+    <OurClients />
+    <FormContact />
+    <FooterContent />
 
     <div class="container-fluid">
       <colombia-news></colombia-news>
-    </div>gi
-
+    </div>
   </div>
 </template>
 
@@ -21,11 +41,11 @@
 import MainNavigation from "./components/MainNavigation";
 import OfertServices from "./components/OfertServices";
 import ColombiaNews from "./components/ColombiaNews";
-import TeamMembers from "./components/TeamMembers";
+import TeamCard from "./components/TeamCard";
 import AboutUs from "./components/AboutUs";
 import OurClients from "./components/OurClients";
-import FormContact from "./components/FormContact"; 
-import FooterContent from "./components/FooterContent"; 
+import FormContact from "./components/FormContact";
+import FooterContent from "./components/FooterContent";
 
 export default {
   name: "App",
@@ -33,12 +53,54 @@ export default {
     MainNavigation,
     OfertServices,
     ColombiaNews,
-    TeamMembers,
+    TeamCard,
     AboutUs,
     OurClients,
     FormContact,
-    FooterContent
-  }
+    FooterContent,
+  },
+  data() {
+    return {
+      equipo: [
+    {        
+        codigo: 1,
+        nombre: "Daniel Eslava",
+        descripcion: "Ingeniero Industrial de la Escuela de Ingenieros con énfasis en proyectos, hobbie trompetista .",
+        rol: "Ingeniero Indistrial / Desarrollador backend",
+        image: "../assets/img/integrantes/daniel"
+    },
+    {
+        codigo: 2,
+        nombre: "Carlos Ruiz",
+        descripcion: "Ingeniero de sistemas de la Universidad Nacional de Colombia, experiencia en uso de framework php (Yii), hobbie bailar salsa.",
+        rol: "Scrum Master",
+        image: "./assets/img/integrantes/carlos.png"
+    },
+    {
+        codigo: 3,
+        nombre: "Michael Arias",
+        descripcion: "Director de proyectos de ingeniería, Ingeniero mecánico de la Universidad Distrital Francisco José de Caldas",
+        rol: "Desarrollador web (Ux-Ui)",
+        image: "../assets/img/integrantes/mike.jpeg"
+    },
+    {
+        codigo: 4,
+        nombre: "Lucas Mera",
+        descripcion: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris condimentum ac elit et accumsan.",
+        rol: "Desarrollador backend",
+        image: "path/to/image"
+    },
+    {
+        codigo: 5,
+        nombre: "Heywin Meneses",
+        descripcion: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris condimentum ac elit et accumsan.",
+        rol: "Desarrollador backend",
+        image: "path/to/image"
+    }
+]
+    };
+  },
+  
 };
 </script>
 
@@ -50,5 +112,8 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+#contenedor{
+  width: 20%;
 }
 </style>
--- src/components/TeamCard.vue
+++ src/components/TeamCard.vue
@@ -1,26 +1,31 @@
 <template>
-    <div section class="page-section bg-light" id="teamCard">
-        <div class="row">
-            <div class="col-lg-4">
-                <div class="team-member">
-                    <img class="mx-auto rounded-circle" src="image" alt="nombre" title="nombre"/>
-                    <h4>Charlitos</h4>
-                    <p class="text-muted">{{rol}}</p>
-                    <p class="">{{descripcion}}</p>
-                    <a class="btn btn-dark btn-social mx-2" href="#!"><i class="fab fa-twitter"></i></a>
-                    <a class="btn btn-dark btn-social mx-2" href="#!"><i class="fab fa-facebook-f"></i></a>
-                    <a class="btn btn-dark btn-social mx-2" href="#!"><i class="fab fa-linkedin-in"></i></a>
-                </div>
-            </div>
-        </div>
-    </div>
+  <div class="team-member">
+    <img
+      class="mx-auto rounded-circle"
+      :src="member.imagen"
+      alt="Avatar"
+      title="Avatar"
+    />
+    <h4>{{ member.nombre }}</h4>
+    <p class="text-muted">{{ member.rol }}</p>
+    <p>{{ member.descripcion }}</p>
+    <a class="btn btn-dark btn-social mx-2" href="#!"
+      ><i class="fab fa-twitter"></i
+    ></a>
+    <a class="btn btn-dark btn-social mx-2" href="#!"
+      ><i class="fab fa-facebook-f"></i
+    ></a>
+    <a class="btn btn-dark btn-social mx-2" href="#!"
+      ><i class="fab fa-linkedin-in"></i
+    ></a>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "TeamCard",
-        props: ['member']
-    };
+export default {
+  name: "TeamCard",
+  props: ["member"]
+};
 </script>
 
 <style scoped>
