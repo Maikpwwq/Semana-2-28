30109199306af9b8413b7d2f0e24b92e31f9674b
Maikpwwq <mariasf@correo.udistrital.edu.co>
2020-12-14 03:31:38 -0500

    TeamCard

--- src/App.vue
+++ src/App.vue
@@ -16,8 +16,28 @@
                   
                 </div>
         </div>
+    </section>    
+    <!-- Team <TeamMembers/> -->
+    <section class="page-section bg-light" id="team">
+            <div class="container">
+                <div class="text-center">
+                    <h2 class="section-heading text-uppercase">Un increible equipo</h2>
+                    <h3 class="section-subheading text-muted">Tripulacion MisionTic2022 #elegidosparatriunfar.</h3>
+                </div>
+                    
+                <div class="row">
+                    <div class="col-3" v-for="(item, index) of equipo" :key="index">
+                        <team-card :member="item"></team-card>
+                    </div>
+                </div>
+                
+                <div class="row">
+                    <div class="col-lg-8 mx-auto text-center">
+                        <p class="large text-muted">El Motivante del desarrollo.</p>
+                    </div>
+                </div>
+            </div>
     </section>
-    <TeamMembers/>
     <AboutUs/>
     <OurClients/>
     <FormContact/>
@@ -29,7 +49,8 @@
 import MainNavigation from "./components/MainNavigation";
 import OfertServices from "./components/OfertServices";
 import ColombiaNews from "./components/ColombiaNews";
-import TeamMembers from "./components/TeamMembers";
+//import TeamMembers from "./components/TeamMembers";
+import TeamCard from "./components/TeamCard";
 import AboutUs from "./components/AboutUs";
 import OurClients from "./components/OurClients";
 import FormContact from "./components/FormContact"; 
@@ -41,12 +62,38 @@ export default {
     MainNavigation,
     OfertServices,
     ColombiaNews,
-    TeamMembers,
+    //TeamMembers,
+    TeamCard,
     AboutUs,
     OurClients,
     FormContact,
     FooterContent
-  }
+  },
+  // Miembros grupo 28
+  data() {
+    return {
+      equipo: [''],
+    };
+  },
+  methods: {
+      getJson: function (){
+          axios.get(
+              "../data/miembrosEquipo.json"
+          )
+          .then((response) => {
+            (this.equipo = response.data),
+            console.log(this.equipo)
+          })
+          .catch(error => {
+            console.log(error)
+            this.errored = true
+          })
+          .finally(() => this.loading = false) 
+      }
+  },
+  mounted() {
+    this.getJson();
+  },
 };
 </script>
 
